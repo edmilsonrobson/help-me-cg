9495ce9ec0185dfb5d98f1c6fdbb452cc7bd9d50
Edmilson <edmilsonrobson@gmail.com>
2016-11-29 00:12:55 -0300

    Adds quaternion calculation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function calculateQuaternions(){
-	
+function calculateQuaternions(){	
+	var w = $('input[name=w]').val();
+	var x = $('input[name=x]').val();
+	var y = $('input[name=y]').val();
+	var z = $('input[name=z]').val();
+
+	var items = [
+			  [w*w + x*x - y*y - z*z, (2*x*y - 2*w*z), 2*x*z + 2*w*y, 0],
+			  [(2*x*y + 2*w*z), (w*w - x*x + y*y - z*z), (2*y*z - 2*w*x), 0 ],
+			  [(2*x*z -  2*w*y), (2*y*z + 2*w*x), (w*w - x*x - y*y + z*z), 0],
+			  [0, 0, 0, (w*w+x*x+y*y+z*z)]
+			];
+	var response = "";
+	for (i = 0 ; i < 4 ; i++){
+		for (j = 0 ; j < 4 ; j++){
+			response = response + " " + items[i][j];
+		}
+		response = response + "\n";
+	}
+
+	alert(response);
 }
